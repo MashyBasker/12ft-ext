92d9a8e6ae90ac9bd332401cdb4fc46aa9a80991
Rishi Kumar <rishikmr177@gmail.com>
2022-12-10 16:23:04 +0530

    fix: redirect

--- src/main.js
+++ src/main.js
@@ -1,16 +1,8 @@
 let browserUrl = window.location.href;
-const button = document.getElementsByClassName("button-2");
+const baseUrl = "https://12ft.io/";
+const button232 = document.getElementsByClassName("button-2");
 
-const searchURl = (browserUrl) => {
-  //   console.log("Redirecting...");
-  window.location("https://12ft.io/");
-  let inputBox = document.getElementsByClassName(
-    "px-4 flex-1 min-w-0 border border-gray-400 border-r-0"
-  );
-  let searchbutton = document.getElementsByClassName(
-    "px-4 py-2 min-w-12 text-sm leading-none font-medium border border-yellow-500 bg-yellow-100 text-yellow-700 hover:bg-yellow-200"
-  );
-  inputBox.value = browserUrl;
-  searchbutton.click();
+const clickingFunction = () => {
+  location.replace(`${baseUrl}+${browserUrl}`);
+  console.log(`${baseUrl}+${browserUrl}`);
 };
-button.addEventListener("click", searchURl(browserUrl));
