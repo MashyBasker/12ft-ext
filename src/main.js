00bd0c1bbe31e9a8bd649822997ceb95b3459f2b
Rishi Kumar <rishikmr177@gmail.com>
2022-12-10 16:07:22 +0530

    fix folder structure and add styles

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-let browserUrl = window.location.href
+let browserUrl = window.location.href;
+const button = document.getElementsByClassName("button-2");
 
 const searchURl = (browserUrl) => {
-    console.log("Redirecting...");
-    window.location("https://12ft.io/");
-    let inputBox = document.getElementsByClassName("px-4 flex-1 min-w-0 border border-gray-400 border-r-0");
-    let searchbutton = document.getElementsByClassName("px-4 py-2 min-w-12 text-sm leading-none font-medium border border-yellow-500 bg-yellow-100 text-yellow-700 hover:bg-yellow-200");
-    inputBox.value = browserUrl;
-    searchbutton.click();
-}
-
-searchURl(browserUrl);
+  //   console.log("Redirecting...");
+  window.location("https://12ft.io/");
+  let inputBox = document.getElementsByClassName(
+    "px-4 flex-1 min-w-0 border border-gray-400 border-r-0"
+  );
+  let searchbutton = document.getElementsByClassName(
+    "px-4 py-2 min-w-12 text-sm leading-none font-medium border border-yellow-500 bg-yellow-100 text-yellow-700 hover:bg-yellow-200"
+  );
+  inputBox.value = browserUrl;
+  searchbutton.click();
+};
+button.addEventListener("click", searchURl(browserUrl));
