5be6f46078adfd68c0abfd0f02e899e2ea0f6160
Rishi Kumar <rishikmr177@gmail.com>
2022-12-11 00:48:49 +0530

    optimize for firefox

--- src/main.js
+++
@@ -1,8 +0,0 @@
-let browserUrl = window.location.href;
-const baseUrl = "https://12ft.io/";
-const button232 = document.getElementsByClassName("button-2");
-
-const clickingFunction = () => {
-  location.replace(`${baseUrl}+${browserUrl}`);
-  console.log(`${baseUrl}+${browserUrl}`);
-};
