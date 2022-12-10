35721391af3cba92b979dd7cdce6b8b7ffccd84e
MashyBasker <basumaharshi10@gmail.com>
2022-12-10 15:40:13 +0530

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+let browserUrl = window.location.href
+
+const searchURl = (browserUrl) => {
+    console.log("Redirecting...");
+    window.location("https://12ft.io/");
+    let inputBox = document.getElementsByClassName("px-4 flex-1 min-w-0 border border-gray-400 border-r-0");
+    let searchbutton = document.getElementsByClassName("px-4 py-2 min-w-12 text-sm leading-none font-medium border border-yellow-500 bg-yellow-100 text-yellow-700 hover:bg-yellow-200");
+    inputBox.value = browserUrl;
+    searchbutton.click();
+}
+
+searchURl(browserUrl);
